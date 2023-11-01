e9827c27e4c684f4a73acc0a330469ac2028f45d
Mally13 <mallymutuku13@gmail.com>
2023-11-01 12:40:52 +0300

    Task 16 Perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,38 @@
 #include "binary_trees.h"
 
+
+/**
+ * max - Returns the max value (a or b)
+ * @a: value 1
+ * @b: value 2
+ * Return: max value
+ */
+int max(int a, int b)
+{
+	return ((a > b) ? a : b);
+}
+
+
+/**
+ * binary_tree_height - Calculates tha height of a tree
+ * @tree: tree to be evaluated
+ * Return: height of the tree
+ */
+int binary_tree_height(const binary_tree_t *tree)
+{
+	int l_height, r_height, height;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	height = 1 + max(l_height, r_height);
+	return (height);
+}
+
+
 /**
  * binary_tree_balance - Measures the balance factor of a binary tree
  * @tree: tree to be measured
@@ -8,28 +41,15 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int bf = 0, l_edges = 0, r_edges = 0;
-	const binary_tree_t *root = tree;
+	int bf = 0, l_height = 0, r_height = 0;
 
-	if (tree)
+	if (tree == NULL)
 	{
-		while (tree->left)
-		{	if (tree->left->right)
-				l_edges += 1;
-			l_edges += 1;
-			tree = tree->left;
-		}
+		return (0);
 	}
-	if (root)
-	{
-		while (root->right)
-		{	if (root->right->left)
-				r_edges += 1;
-			r_edges += 1;
-			root = root->right;
-		}
-	}
-	bf = l_edges - r_edges;
+	l_height = binary_tree_height(tree->left);
+	r_height = binary_tree_height(tree->right);
+	bf = l_height - r_height;
 	return (bf);
 }
 
@@ -42,11 +62,10 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int perfect  = 0, bal;
+	int perfect = 0, bal;
 
 	if (tree == NULL)
 		return (0);
-
 	bal = binary_tree_balance(tree);
 	if (bal == 0)
 		perfect = 1;
