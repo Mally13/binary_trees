83ee39c303cabf06dc51659e59be2b2dab83e7f1
Mally13 <mallymutuku13@gmail.com>
2023-11-01 15:39:55 +0300

    Task 16 perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -40,13 +40,16 @@ int binary_tree_height(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	int perfect = 0, l_height, r_height;
+	int l_height, r_height;
 
 	if (tree == NULL)
 		return (0);
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
-	if (l_height == r_height)
-		perfect = 1;
-	return (perfect);
+	if (l_height != r_height)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+	return (binary_tree_is_perfect(tree->left) &&
+			binary_tree_is_perfect(tree->right));
 }
