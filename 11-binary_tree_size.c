1856c76cb8fc7d8415334c2707b2ce4435bb36cb
Mally13 <mallymutuku13@gmail.com>
2023-11-02 17:59:31 +0300

    Task 11 - size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -12,7 +12,7 @@ size_t binary_tree_size(const binary_tree_t *tree)
 	{
 		size = 1;
 		size += tree->left ? binary_tree_size(tree->left) : 0;
-		size += tree->right ? binary_tree_size(tree->right): 0;
+		size += tree->right ? binary_tree_size(tree->right) : 0;
 		return (size);
 	}
 	return (0);
